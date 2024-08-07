f510fddf9ec2b1fe21ad21173f7c2e21556c9a36
Gabriel13 <gabrielesp7724@gmail.com>
2024-08-07 18:19:50 -0500

    [FIX]: Navbar FIX + dependecia del pom

--- src/main/java/com/taller1/taller/TallerApplication.java
+++ src/main/java/com/taller1/taller/TallerApplication.java
@@ -2,6 +2,7 @@ package com.taller1.taller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TallerApplication {
@@ -10,5 +11,4 @@ public class TallerApplication {
 		SpringApplication.run(TallerApplication.class, args);
 	}
 
-	
 }
